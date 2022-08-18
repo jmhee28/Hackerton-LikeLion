98a8f29c8fea351c75a5313bc112d927b09939fc
Junhyeong Jo <junhyeong.tech@gmail.com>
2022-08-18 13:18:01 +0900

    채팅222

--- artist/artistapp/views.py
+++ artist/artistapp/views.py
@@ -22,7 +22,7 @@ def room(request, room):
 
 def checkview(request):
     room = request.POST['room_name']
-    username = request.POST['username']
+    username =  request.POST['username']
 
     if Room.objects.filter(name=room).exists():
         return redirect('/'+room+'/?username='+username)
